9e3db8430cd32253ef82f7175e907165ddba8092
michal-wolski <michal.wolski130@gmail.com>
2022-07-23 17:13:51 +0200

    Update Actions.js

--- src/components/Redux/Actions.js
+++ src/components/Redux/Actions.js
@@ -1,12 +1,3 @@
 import { createAction } from '@reduxjs/toolkit';
-import { nanoid } from 'nanoid';
 
-export const deleteContacts = createAction('contacts/delete');
-export const changeFilter = createAction('contacts/changeFilter');
-export const addContacts = createAction('contacts/add', (name, phone) => ({
-  payload: {
-    id: nanoid(),
-    name,
-    phone,
-  },
-}));
+export const changeFilter = createAction('filter/change');
